8cb40e0cb3704e460e6342959c5e0a8baa1c0938
hmongkey12 <khammadaolee@gmail.com>
2023-08-23 18:26:09 -0700

    Updated link and link managers to have static typing

--- src/link.py
+++ src/link.py
@@ -1,13 +1,17 @@
-class Link:
-    def __init__(self, router1, router2, cost):
-        self.router1 = router1
-        self.router2 = router2
-        self.cost = cost
+from typing import List
+from router import Router
 
-    def connect_routers(self):
+
+class Link:
+    def __init__(self, router1: Router, router2: Router, cost: int) -> None:
+        self.router1: Router = router1
+        self.router2: Router = router2
+        self.cost: int = cost
+
+    def connect_routers(self) -> None:
         self.router1.add_neighbor(self.router2.router_id, self.cost)
         self.router2.add_neighbor(self.router1.router_id, self.cost)
 
-    def disconnect_routers(self):
+    def disconnect_routers(self) -> None:
         self.router1.remove_neighbor(self.router2.router_id)
         self.router2.remove_neighbor(self.router1.router_id)
--- src/managers/link_manager.py
+++ src/managers/link_manager.py
@@ -1,11 +1,13 @@
+from typing import List
+from router import Router
 from link import Link
 
 
 class LinkManager:
-    def __init__(self):
-        self.links = []
+    def __init__(self) -> None:
+        self.links: List[Link] = []
 
-    def create_link(self, router1, router2, cost):
-        link = Link(router1, router2, cost)
+    def create_link(self, router1: Router, router2: Router, cost: int) -> None:
+        link: Link = Link(router1, router2, cost)
         link.connect_routers()
         self.links.append(link)
